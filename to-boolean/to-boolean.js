24c1c9f13b072921a25fb5e5074627d9e7b28ba2
getlarge <ed@getlarge.eu>
2020-06-21 18:59:24 +0200

    create to-boolean node

---
+++ to-boolean/to-boolean.js
@@ -0,0 +1,94 @@
+module.exports = function (RED) {
+  const { isValidTopic } = require('../helpers');
+
+  function ToBoolean(config) {
+    RED.nodes.createNode(this, config);
+
+    const node = this;
+    const { useTopic, topic, resource } = config;
+
+    function inputsValid(msg) {
+      const { payload, resource, topic } = msg;
+      if ((!useTopic && !resource) || (useTopic && !topic) || !payload || !payload.resources) {
+        node.error(RED._('aloes.errors.missing-input'));
+        return false;
+      } else if (useTopic && topic && !isValidTopic(topic)) {
+        node.error(RED._('aloes.errors.invalid-topic'));
+        return false;
+      }
+      return true;
+    }
+
+    node.on('input', function (msg, send, done) {
+      try {
+        send =
+          send ||
+          function () {
+            node.send.apply(node, arguments);
+          };
+
+        if (topic) {
+          msg.topic = topic;
+        }
+        if (resource) {
+          msg.resource = resource;
+        }
+
+        if (!inputsValid(msg)) {
+          done();
+          return;
+        }
+
+        // if (useTopic && msg.topic) {
+        //   const parts = msg.topic.split('/');
+        //   msg.collection = parts[1];
+        //   msg.method = parts[2];
+        //   msg.instanceProperty = parts[3];
+        //   if (msg.collection === COLLECTIONS.SENSOR) {
+        //     msg.resource = parts[6];
+        //   }
+        // }
+
+        let value = msg.payload.resources[resourceId];
+        if (typeof value !== 'boolean') {
+          if (typeof value === 'object' && value.type && value.data) {
+            value = Buffer.from(value.data).toString('utf-8');
+          } else if (Buffer.isBuffer(value)) {
+            value = value.toString('utf-8');
+          } else if (value instanceof Array) {
+            value = Buffer.from(value).toString('utf-8');
+          }
+
+          if (typeof value === 'string') {
+            if (msg.payload === 'true' || msg.payload === '1') {
+              msg.payload = true;
+            } else if (msg.payload === 'false' || msg.payload === '0') {
+              msg.payload = false;
+            } else {
+              msg.payload = Boolean(value);
+            }
+          } else if (typeof value === 'number') {
+            msg.payload = Boolean(value);
+          } else {
+            throw new Error('No value found');
+          }
+        } else {
+          msg.payload = value;
+        }
+
+        send(msg);
+
+        if (done) {
+          done();
+        }
+      } catch (error) {
+        if (done) {
+          done(error);
+        } else {
+          node.error(error, msg);
+        }
+      }
+    });
+  }
+  RED.nodes.registerType('to-boolean', ToBoolean);
+};
